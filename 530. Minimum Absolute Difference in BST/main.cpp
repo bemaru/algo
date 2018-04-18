103337bcef471a7e3689bd70aa2614bfbfaa237c
bemaru <kwakghoon@gmail.com>
2018-04-18 20:49:20 +0900

    solve 530. Minimum Absolute Difference in BST

---
+++ 530. Minimum Absolute Difference in BST/main.cpp
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+
+
+class Solution {
+    int result = 9999;
+    int prev = 9999;
+    public:
+    int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+        return result;
+    }
+
+    void dfs(TreeNode* node) 
+    {
+        if (node == NULL)
+            return;
+
+        dfs(node->left);
+        result = min(abs(prev - node->val),result);
+        prev = node->val;
+        dfs(node->right);
+    }
+};
+
+
+
+void main()
+{
+    //string str="123";
+    
+
+    string str = "[18,7,26,3,12,21,31]";
+    printf("%d\n", Solution().getMinimumDifference(stringToTreeNode(str)));
+    return;
+}
