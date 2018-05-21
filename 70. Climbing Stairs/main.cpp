422f5877d6480b0f9ebe4466b50a30183238bcf6
bemaru <kwakghoon@gmail.com>
2018-05-21 13:00:20 +0900

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <vector>
+
+std::vector<int> result(9999, -1);
+class Solution {
+
+    public:
+    
+    Solution()
+    {
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 2;
+    }
+    
+    int climbStairs(int n) 
+    {
+        if (result[n] == -1)
+            return result[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        else 
+            return result[n];
+    }
+};
+
+void main()
+{   
+    int n = 4;
+    printf("%d\n", Solution().climbStairs(n));
+    return;
+}
