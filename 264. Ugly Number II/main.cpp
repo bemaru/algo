fcead173a959299a71a70e24ad14f06b08f19d14
bemaru <kwakghoon@gmail.com>
2018-05-21 13:00:04 +0900

    264. Ugly Number II

---
+++ 264. Ugly Number II/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+class Solution {
+    public:
+    int nthUglyNumber(int n) {
+
+        return 0;
+    }
+};
+
+void main()
+{
+    std::cout << Solution().nthUglyNumber(10);
+    return;
+}
