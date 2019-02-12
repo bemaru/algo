9c0d4d0aadb1fddc8ee1efa8d7283c62151c09df
bemaru <kwakghoon@gmail.com>
2019-02-12 21:41:53 +0900

    boj 11727 2 x n 타일2

---
+++ BOJ/03_2xnTile2/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int input;
+    scanf("%d", &input);
+
+    int dp[1000] = {};
+    dp[0] = 1, dp[1] = 1;
+
+    for (int i = 2; i <= input; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]*2) % 10007;
+    }
+
+    printf("%d", dp[input]);
+    return 0;
+}
