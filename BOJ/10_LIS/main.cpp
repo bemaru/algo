802afe3716aba16ffae3c5d1f072ed3067148fb7
bemaru <kwakghoon@gmail.com>
2019-05-16 12:53:07 +0900

    solve boj.kr/11053 longest increasing subsequence

---
+++ BOJ/10_LIS/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    vector<int> arr(n);
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    vector<int> dp(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        dp[i] = 1; // 원소가 있다면 길이는 1부터 시작
+        for (int j = 0; j < i; j++) // 자신 이전의 값을 순회
+        {
+            if (arr[i] > arr[j])    // 이전 값이 자신보다 작다면
+            {
+                dp[i] = max(dp[i], dp[j] + 1); // 이전 값 인덱스의 dp를 증가시켜 갱신한다. 단, 가장 큰 dp에서.
+            }
+        }
+    }
+    
+    cout << *max_element(dp.begin(), dp.end());
+    
+    return 0;
+}
